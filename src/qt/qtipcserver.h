8b4a60a186a91f5777d3cc623bb9a4a3aa715eda
smoke <smokeandcrypto@gmail.com>
2021-08-04 20:49:03 -0700

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitcoinBuck-Qt message queue name
+#define BITCOINBUCKURI_QUEUE_NAME "BitcoinBuckURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
